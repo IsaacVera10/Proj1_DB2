591d16ba59d0d7596e2836ab2c165636aeefa349
AlexandroChamochumbi <85258069+AlexandroChamochumbi@users.noreply.github.com>
2023-09-26 13:40:51 -0500

    Add files via upload

--- avl.h
+++ avl.h
@@ -3,379 +3,353 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
-
+#include <cstring>
 using namespace std;
 
-struct Record
-{
-    int cod;
-    char nombre[12];
-    int ciclo;
+struct MovieRecord {
+    char id[7];
+    char prenda[10];
+    char genero[1]; 
+    float precio;
+    char stock[4]; 
+    char marca[7];
     long left = -1, right = -1;
     int height = 1; 
 
-    void showData()
-    {
-        cout << "\nCodigo: " << cod;
-        cout << "\nNombre: " << nombre;
-        cout << "\nCiclo : " << ciclo << endl;
+		MovieRecord() {}
+    MovieRecord(string id_, string prenda_, string genero_, float precio_, string stock_, string marca_) {
+        for (int i = 0; i < 7; i++) {
+            this->id[i] = id_[i];
+        }
+        for (int i = 0; i < 10; i++) {
+            this->prenda[i] = prenda_[i];
+        }
+        for (int i = 0; i < 1; i++) {
+            this->genero[i] = genero_[i];
+        }
+        this->precio = precio_;
+        for (int i = 0; i < 4; i++) {
+            this->stock[i] = stock_[i];
+        }
+        for (int i = 0; i < 7; i++) {
+            this->marca[i] = marca_[i];
+        }
     }
+    void showData() {
+    if (id[0] == '\0') {
+        return; // Skip displaying data for empty nodes
+    }
+    cout << "\nID: " << id;
+    cout << "\nPrenda: " << prenda;
+    cout << "\nGénero: " << genero;
+    cout << "\nPrecio: " << precio;
+    cout << "\nStock: " << stock;
+    cout << "\nMarca: " << marca << endl;
+}
+
 };
 
-class AVLFile
-{
+class AVLFile {
 private:
     string filename;
     long pos_root;
 
 public:
-    AVLFile(string filename)
-    {
+    AVLFile(string filename) {
         this->filename = filename;
         fstream file(filename, ios::binary | ios::in | ios::out);
         this->pos_root = -1;
-        if (!file.is_open())
-        {
+        if (!file.is_open()) {
             file.close();
             fstream newFile(filename, ios::binary | ios::out);
             newFile.close();
         }
-        else
-        {
+        else {
             file.read(reinterpret_cast<char *>(&pos_root), sizeof(long));
             file.close();
         }
     }
 
-    ~AVLFile()
-    {
+    ~AVLFile() {
         fstream file(filename, ios::binary | ios::in | ios::out);
         file.write(reinterpret_cast<char *>(&pos_root), sizeof(long));
         file.close();
     }
 
-    Record find(int key)
-    {
-        return find(pos_root, key);
+    MovieRecord find(char id[7]) {
+        return find(pos_root, id);
     }
 
-    void insert(Record record)
-    {
+    void insert(MovieRecord record) {
         insert(pos_root, record);
     }
 
-    void remove(int key)
-    {
-        remove(pos_root, key);
+    void remove(char *id) {
+        remove(pos_root, id);
     }
 
-    vector<Record> inorder()
-    {
+    vector<MovieRecord> inorder() {
         return inorder(pos_root);
     }
 
-    vector<Record> range_search(int low, int high)
-    {
-        vector<Record> result;
-        range_search(pos_root, low, high, result);
+    vector<MovieRecord> range_search(char start[7], char end[7]) {
+        vector<MovieRecord> result;
+        range_search(pos_root, start, end, result);
         return result;
     }
 
 private:
-    Record find(long pos_node, int key)
-    {
+    MovieRecord find(long pos_node, char id[7]) {
         ifstream file(filename, ios::binary);
-        if (!file)
-        {
+        if (!file) {
             cerr << "Error" << endl;
-            return Record{-1, "", -1};
+            return MovieRecord{"", "", "", 0.0, "", ""};
         }
-        file.seekg(pos_node * sizeof(Record));
-        Record node;
-        file.read(reinterpret_cast<char *>(&node), sizeof(Record));
+        file.seekg(pos_node * sizeof(MovieRecord));
+        MovieRecord node;
+        file.read(reinterpret_cast<char *>(&node), sizeof(MovieRecord));
         file.close();
-        if (node.cod == key)
-        {
+        if (strcmp(node.id, id) == 0) {
             return node;
         }
-        else if (key < node.cod && node.left != -1)
-        {
-            return find(node.left, key);
+        else if (strcmp(id, node.id) < 0 && node.left != -1) {
+            return find(node.left, id);
         }
-        else if (key > node.cod && node.right != -1)
-        {
-            return find(node.right, key);
+        else if (strcmp(id, node.id) > 0 && node.right != -1) {
+            return find(node.right, id);
         }
-        return Record{-1, "", -1};
+        return MovieRecord{"", "", "", 0.0, "", ""};
     }
 
-    void insert(long &pos_node, Record record)
-    {
-        if (pos_node == -1)
-        {
+    void insert(long &pos_node, MovieRecord record) {
+        if (pos_node == -1) {
             fstream file(filename, ios::binary | ios::in | ios::out);
             file.seekp(0, ios::end);
-            pos_node = file.tellp() / sizeof(Record);
-            file.write(reinterpret_cast<char *>(&record), sizeof(Record));
+            pos_node = file.tellp() / sizeof(MovieRecord);
+            file.write(reinterpret_cast<char *>(&record), sizeof(MovieRecord));
             file.close();
         }
-        else
-        {
+        else {
             fstream file(filename, ios::binary | ios::in | ios::out);
-            file.seekg(pos_node * sizeof(Record));
-            Record node;
-            file.read(reinterpret_cast<char *>(&node), sizeof(Record));
-            if (record.cod < node.cod)
-            {
-                if (node.left == -1)
-                {
+            file.seekg(pos_node * sizeof(MovieRecord));
+            MovieRecord node;
+            file.read(reinterpret_cast<char *>(&node), sizeof(MovieRecord));
+            if (strcmp(record.id, node.id) < 0) {
+                if (node.left == -1) {
                     file.seekp(0, ios::end);
-                    node.left = file.tellp() / sizeof(Record);
-                    file.write(reinterpret_cast<char *>(&record), sizeof(Record));
-                    file.seekp(pos_node * sizeof(Record));
-                    file.write(reinterpret_cast<char *>(&node), sizeof(Record));
+                    node.left = file.tellp() / sizeof(MovieRecord);
+                    file.write(reinterpret_cast<char *>(&record), sizeof(MovieRecord));
+                    file.seekp(pos_node * sizeof(MovieRecord));
+                    file.write(reinterpret_cast<char *>(&node), sizeof(MovieRecord));
                     file.close();
                 }
-                else
-                {
+                else {
                     insert(node.left, record);
                 }
             }
-            else
-            {
-                if (node.right == -1)
-                {
+            else {
+                if (node.right == -1) {
                     file.seekp(0, ios::end);
-                    node.right = file.tellp() / sizeof(Record);
-                    file.write(reinterpret_cast<char *>(&record), sizeof(Record));
-                    file.seekp(pos_node * sizeof(Record));
-                    file.write(reinterpret_cast<char *>(&node), sizeof(Record));
+                    node.right = file.tellp() / sizeof(MovieRecord);
+                    file.write(reinterpret_cast<char *>(&record), sizeof(MovieRecord));
+                    file.seekp(pos_node * sizeof(MovieRecord));
+                    file.write(reinterpret_cast<char *>(&node), sizeof(MovieRecord));
                     file.close();
                 }
-                else
-                {
+                else {
                     insert(node.right, record);
                 }
             }
+            // Update the height of the current node
             node.height = 1 + max(getHeight(node.left), getHeight(node.right));
+            // Perform balancing
             balance(pos_node);
         }
     }
-
-    void remove(long &pos_node, int key)
-{
-    if (pos_node == -1)
-    {
-        return;
+void remove(long &pos_node, const char *key) {
+    if (pos_node == -1) {
+        return; // Key not found, nothing to delete
     }
 
-    Record node = getRecord(pos_node);
+    MovieRecord node = getRecord(pos_node);
 
-    if (key < node.cod)
-    {
+    // Compare characters individually
+    int i = 0;
+    while (key[i] != '\0' && node.id[i] != '\0' && key[i] == node.id[i]) {
+        i++;
+    }
+
+    if (key[i] == '\0' && node.id[i] == '\0') {
+        // Node with the key to be deleted found
+
+        // Node with one child or no child
+        if (node.left == -1 || node.right == -1) {
+            long temp = (node.left == -1) ? node.right : node.left;
+
+            // No child case
+            if (temp == -1) {
+                temp = pos_node;
+                pos_node = -1; // Set the current node to null
+            } else {
+                node = getRecord(temp); // Copy the contents of the child
+                pos_node = temp;
+            }
+        } else {
+            // Node with two children: get the in-order successor
+            long successorPos = findMin(node.right);
+            MovieRecord &successor = getRecord(successorPos);
+
+            // Copy the in-order successor's data to this node
+            strcpy(node.id, successor.id);
+            strcpy(node.prenda, successor.prenda);
+            strcpy(node.genero, successor.genero);
+            node.precio = successor.precio;
+            strcpy(node.stock, successor.stock);
+            strcpy(node.marca, successor.marca);
+
+            // Delete the in-order successor recursively
+            remove(node.right, successor.id);
+        }
+    } else if (key[i] < node.id[i]) {
         remove(node.left, key);
-    }
-    else if (key > node.cod)
-    {
+    } else {
         remove(node.right, key);
     }
-    else
-    {
-        if (node.left == -1 && node.right == -1)
-        {
-       
-            pos_node = -1; 
 
-            fstream file(filename, ios::binary | ios::in | ios::out);
-            file.seekp(pos_node * sizeof(Record));
-            Record deletedNode;
-            file.write(reinterpret_cast<char *>(&deletedNode), sizeof(Record));
-            file.close();
-        }
-        else if (node.left == -1 || node.right == -1)
-        {
-
-            pos_node = (node.left == -1) ? node.right : node.left; 
-            fstream file(filename, ios::binary | ios::in | ios::out);
-            file.seekp(pos_node * sizeof(Record));
-            Record deletedNode;
-            file.write(reinterpret_cast<char *>(&deletedNode), sizeof(Record));
-            file.close();
-        }
-        else
-        {
-           
-            long successorPos = findMin(node.right); 
-            Record successor = getRecord(successorPos);
-            node.cod = successor.cod; 
-            remove(node.right, successor.cod); 
-        }
+    if (pos_node == -1) {
+        return; // The tree is empty or the node was removed
     }
 
-    
+    // Update height of the current node
+    node.height = 1 + max(getHeight(node.left), getHeight(node.right));
+
+    // Rebalance the tree
     balance(pos_node);
 }
-long findMin(long pos_node)
-{
-    while (getRecord(pos_node).left != -1)
-    {
-        pos_node = getRecord(pos_node).left;
-    }
-    return pos_node;
-}
-Record& getRecord(long pos)
-{
-    ifstream file(filename, ios::binary);
-    static Record node; 
-    file.seekg(pos * sizeof(Record));
-    file.read(reinterpret_cast<char *>(&node), sizeof(Record));
-    file.close();
-    return node;
-}
 
 
-    long minValueNode(long pos_node)
-    {
-        Record node = getRecord(pos_node);
-        while (node.left != -1)
-        {
-            node = getRecord(node.left);
+
+    long findMin(long pos_node) {
+        while (getRecord(pos_node).left != -1) {
+            pos_node = getRecord(pos_node).left;
         }
         return pos_node;
     }
 
-    int getHeight(long pos_node)
-    {
-        if (pos_node == -1)
-        {
+    MovieRecord& getRecord(long pos) {
+        ifstream file(filename, ios::binary);
+        static MovieRecord node; // A static variable to store the result
+        file.seekg(pos * sizeof(MovieRecord));
+        file.read(reinterpret_cast<char *>(&node), sizeof(MovieRecord));
+        file.close();
+        return node;
+    }
+
+    int getHeight(long pos_node) {
+        if (pos_node == -1) {
             return 0;
         }
         return getRecord(pos_node).height;
     }
 
-    int getBalance(long pos_node)
-    {
-        if (pos_node == -1)
-        {
+    int getBalance(long pos_node) {
+        if (pos_node == -1) {
             return 0;
         }
         return getHeight(getRecord(pos_node).left) - getHeight(getRecord(pos_node).right);
     }
 
-    void balance(long &pos_node)
-{
-    int balance = getBalance(pos_node);
+    void balance(long &pos_node) {
+        int balance = getBalance(pos_node);
 
-    // Left Heavy
-    if (balance > 1)
-    {
-        long leftChild = getRecord(pos_node).left;
-        // Left-Right Case
-        if (getBalance(leftChild) < 0)
-        {
-            leftRotate(leftChild);
+        // Left Heavy
+        if (balance > 1) {
+            long leftChild = getRecord(pos_node).left;
+            // Left-Right Case
+            if (getBalance(leftChild) < 0) {
+                leftRotate(leftChild);
+            }
+            rightRotate(pos_node);
         }
-        rightRotate(pos_node);
-    }
-    // Right Heavy
-    else if (balance < -1)
-    {
-        long rightChild = getRecord(pos_node).right;
-        // Right-Left Case
-        if (getBalance(rightChild) > 0)
-        {
-            rightRotate(rightChild);
-        }
-        leftRotate(pos_node);
-    }
-}
-
-void leftRotate(long &pos_node)
-{
-    long newRoot = getRecord(pos_node).right;
-    long &parentNode = pos_node;
-    long leftSubtree = getRecord(newRoot).left;
-
-    Record &parentRecord = getRecord(parentNode);
-    Record &newRootRecord = getRecord(newRoot);
-
-    newRootRecord.left = parentNode;
-    parentRecord.right = leftSubtree;
-
-    // Update heights
-    parentRecord.height = 1 + max(getHeight(parentRecord.left), getHeight(parentRecord.right));
-    newRootRecord.height = 1 + max(getHeight(newRootRecord.left), getHeight(newRootRecord.right));
-
-    parentNode = newRoot;
-}
-
-void rightRotate(long &pos_node)
-{
-    long newRoot = getRecord(pos_node).left;
-    long &parentNode = pos_node;
-    long rightSubtree = getRecord(newRoot).right;
-
-    Record &parentRecord = getRecord(parentNode);
-    Record &newRootRecord = getRecord(newRoot);
-
-    newRootRecord.right = parentNode;
-    parentRecord.left = rightSubtree;
-
-    // Update heights
-    parentRecord.height = 1 + max(getHeight(parentRecord.left), getHeight(parentRecord.right));
-    newRootRecord.height = 1 + max(getHeight(newRootRecord.left), getHeight(newRootRecord.right));
-
-    parentNode = newRoot;
-}
-
-
-    void range_search(long pos_node, int low, int high, vector<Record> &result)
-    {
-        if (pos_node == -1)
-        {
-            return;
-        }
-        Record node = getRecord(pos_node);
-        if (node.cod >= low && node.cod <= high)
-        {
-            result.push_back(node);
-        }
-        if (node.cod >= low)
-        {
-            range_search(node.left, low, high, result);
-        }
-        if (node.cod <= high)
-        {
-            range_search(node.right, low, high, result);
+        // Right Heavy
+        else if (balance < -1) {
+            long rightChild = getRecord(pos_node).right;
+            // Right-Left Case
+            if (getBalance(rightChild) > 0) {
+                rightRotate(rightChild);
+            }
+            leftRotate(pos_node);
         }
     }
 
-    vector<Record> inorder(long pos_node)
-    {
-        ifstream file(filename, ios::binary);
-        if (!file.is_open())
-        {
-            cerr << "No se pudo abrir el archivo." << endl;
-            return vector<Record>();
+    void leftRotate(long &pos_node) {
+        long newRoot = getRecord(pos_node).right;
+        long &parentNode = pos_node;
+        long leftSubtree = getRecord(newRoot).left;
+
+        MovieRecord &parentRecord = getRecord(parentNode);
+        MovieRecord &newRootRecord = getRecord(newRoot);
+
+        newRootRecord.left = parentNode; // Assign the values directly
+        parentRecord.right = leftSubtree; // Assign the values directly
+
+        // Update heights
+        parentRecord.height = 1 + max(getHeight(parentRecord.left), getHeight(parentRecord.right));
+        newRootRecord.height = 1 + max(getHeight(newRootRecord.left), getHeight(newRootRecord.right));
+
+        parentNode = newRoot;
+    }
+
+    void rightRotate(long &pos_node) {
+        long newRoot = getRecord(pos_node).left;
+        long &parentNode = pos_node;
+        long rightSubtree = getRecord(newRoot).right;
+
+        MovieRecord &parentRecord = getRecord(parentNode);
+        MovieRecord &newRootRecord = getRecord(newRoot);
+
+        newRootRecord.right = parentNode; // Assign the values directly
+        parentRecord.left = rightSubtree; // Assign the values directly
+
+        // Update heights
+        parentRecord.height = 1 + max(getHeight(parentRecord.left), getHeight(parentRecord.right));
+        newRootRecord.height = 1 + max(getHeight(newRootRecord.left), getHeight(newRootRecord.right));
+
+        parentNode = newRoot;
+    }
+
+   void range_search(long pos_node, const char *start, const char *end, vector<MovieRecord> &result) {
+    if (pos_node == -1) {
+        return;
+    }
+    MovieRecord node = getRecord(pos_node);
+    int id_as_int = atoi(node.id);
+    int start_as_int = atoi(start);
+    int end_as_int = atoi(end);
+
+    if (id_as_int >= start_as_int && id_as_int <= end_as_int) {
+        result.push_back(node);
+    }
+    if (id_as_int > start_as_int) {
+        range_search(node.left, start, end, result);
+    }
+    if (id_as_int < end_as_int) {
+        range_search(node.right, start, end, result);
+    }
+}
+
+
+
+    vector<MovieRecord> inorder(long pos_node) {
+        if (pos_node == -1) {
+            return vector<MovieRecord>(); // Return an empty vector when the node is empty.
         }
-        vector<Record> result;
-        if (pos_node == -1)
-        {
-            file.close();
-            return result;
-        }
-        Record node;
-        file.seekg(pos_node * sizeof(Record));
-        file.read(reinterpret_cast<char *>(&node), sizeof(Record));
-        file.close();
-        if (node.cod == -1)
-        {
-            return result;
-        }
-        vector<Record> leftSubtree = inorder(node.left);
+        MovieRecord node = getRecord(pos_node);
+        vector<MovieRecord> result;
+        vector<MovieRecord> leftSubtree = inorder(node.left);
         result.insert(result.end(), leftSubtree.begin(), leftSubtree.end());
         result.push_back(node);
-        vector<Record> rightSubtree = inorder(node.right);
+        vector<MovieRecord> rightSubtree = inorder(node.right);
         result.insert(result.end(), rightSubtree.begin(), rightSubtree.end());
         return result;
     }
